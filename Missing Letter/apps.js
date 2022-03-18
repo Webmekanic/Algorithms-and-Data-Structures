c98f4eb73f7dc59c3975cc65c08996bccf9e2c59
aniebodavid <aniebovictor001@gmail.com>
2022-03-18 13:31:03 -0700

    Solve Missing Letter Algorithm

---
+++ Missing Letter/apps.js
@@ -0,0 +1,31 @@
+// Missing letters
+function fearNotLetter(str) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz"
+  let startingpoint = alphabet.indexOf(str[0])
+  for (let i = 0; i < str.length; i++) {
+    // console.log(alphabet[startingpoint + i])
+    if (str[i] !== alphabet[startingpoint + i]) {
+      return alphabet[startingpoint + i]
+    }
+  }
+
+  return undefined
+
+  // return str
+}
+
+console.log(fearNotLetter("abce"))
+
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+
+// fearNotLetter("abce") should return the string d.
+
+// fearNotLetter("abcdefghjklmno") should return the string i.
+
+// fearNotLetter("stvwx") should return the string u.
+
+// fearNotLetter("bcdf") should return the string e.
+
+// fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
