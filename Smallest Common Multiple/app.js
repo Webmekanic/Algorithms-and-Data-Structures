d5ed4fd10bd2a72948f769757274bfc701f203ac
aniebodavid <aniebovictor001@gmail.com>
2022-03-31 02:24:17 -0700

    Add Smallest common multiple algorithm

--- Smallest Common Multiple/app.js
+++ Smallest Common Multiple/app.js
@@ -5,7 +5,7 @@
 // For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
 
 function smallestCommons(arr) {
-  return arr
+  // return arr
 }
 
 smallestCommons([1, 5])
