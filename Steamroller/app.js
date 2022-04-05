d5abf8a9aed58d7e8f97e9a826db21b4c5af494c
aniebodavid <aniebovictor001@gmail.com>
2022-04-05 08:35:02 -0700

    Solved Steamroller Algorithms

--- Steamroller/app.js
+++ Steamroller/app.js
@@ -1,10 +1,18 @@
 // Flatten a nested array. You must account for varying levels of nesting.
 
 function steamrollArray(arr) {
-  
+  let stroll = []
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      stroll = stroll.concat(steamrollArray(arr[i]))
+    } else {
+      stroll.push(arr[i])
+    }
+  }
+  return stroll
 }
 
-steamrollArray([1, [2], [3, [[4]]]])
+console.log(steamrollArray([1, {}, [3, [[4]]]]))
 
 // steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
 
