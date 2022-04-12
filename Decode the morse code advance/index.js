19e0fefa8b3c0f36d52925bd16954eb0a49cb468
aniebodavid <aniebovictor001@gmail.com>
2022-04-12 09:50:08 -0700

    Add decode morse code advance kata

--- Decode the morse code advance/index.js
+++ Decode the morse code advance/index.js
@@ -32,3 +32,27 @@
 // All the test strings would be valid to the point that they could be reliably decoded as described above, so you may skip checking for errors and exceptions, just do your best in figuring out what the message is!
 
 // Good luck!
+
+var decodeBits = function (bits) {
+  // ToDo: Accept 0's and 1's, return dots, dashes and spaces
+  return bits
+    .replace("111", "-")
+    .replace("000", " ")
+    .replace("1", ".")
+    .replace("0", "")
+}
+
+var decodeMorse = function (morseCode) {
+  // ToDo: Accept dots, dashes and spaces, return human-readable message
+  return morseCode
+    .replace(".", MORSE_CODE["."])
+    .replace("-", MORSE_CODE["-"])
+    .replace(" ", "")
+}
+
+// Test
+decodeMorse(
+  decodeBits(
+    "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"
+  )
+) //  => 'HEY JUDE'
