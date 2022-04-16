82b527aae698faaa6c7e621a0d171c5f9764b3c1
aniebodavid <aniebovictor001@gmail.com>
2022-04-16 07:53:29 -0700

    solve phone number validator

--- Telephone Number Validator/app.js
+++ Telephone Number Validator/app.js
@@ -1,5 +1,19 @@
 function telephoneCheck(str) {
-  return true
+  let re = /^\(?\d{1,3}\)?[-. ]?\d{3}[-. ]?\d{4}$/
+  if (re.test(str)) {
+    return true
+  } else {
+    return false
+  }
 }
 
-telephoneCheck("555-555-5555")
+console.log(telephoneCheck("1 555 555 5555"))
+
+console.log(telephoneCheck("555-555-5555"))
+
+console.log(telephoneCheck("(555)555-5555"))
+
+console.log(telephoneCheck("(555) 555-5555"))
+
+console.log(telephoneCheck("555 555 5555"))
+console.log(telephoneCheck("555555555"))
