debf973b97d17169e6a50d5a6612874455ebe87d
aniebodavid <aniebovictor001@gmail.com>
2022-04-17 08:03:44 -0700

    solve caesar cipher

--- Caesars Cipher/app.js
+++ Caesars Cipher/app.js
@@ -7,38 +7,24 @@
 // All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 
 function rot13(str) {
-  let newStr = ""
-  let regex = /[A-Z]/
-  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let strArray = str.split("")
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].match(regex)) {
-      //if the ith value of str is a character A-Z:
-      let index = alphabet.indexOf(str[i]) //using the current letter, find its index position from the alphabet
-
-      let newIndex = index + 13 //shift the index by adding 13
-      if (newIndex > 26) {
-        //if the shifted value goes past the letters of the alphabet...
-        newIndex = newIndex - 26 //...subtract the alphabet length from the value...
-        newStr += alphabet[newIndex] //...and start from the beginning of the alphabet. Add the character to the new string.
-      } else if (newIndex == 26) {
-        //should combine this else if with the if above:  newIndex >= 26.
-        newStr += alphabet[0]
-      } else {
-        //if under 26, then just add character to new string
-        newStr += alphabet[newIndex]
-      }
-    } else {
-      //if this isn't a character A-Z, then just pass it on to the new string
-      newStr += str[i]
-    }
+  for (let i = 0; i < strArray.length; i++) {
+    let code = strArray[i].charCodeAt(0)
+    strArray[i] =
+      code < 65 || code > 90
+        ? strArray[i]
+        : code >= 78 && code <= 90
+        ? String.fromCharCode(code - 13)
+        : String.fromCharCode(code + 13)
   }
-  console.log(newStr)
+
+  return strArray.join("")
   //return newStr; //original value for freeCodeCamp.org answer
 }
 
 // rot13("SERR CVMMN!")
-rot13("SERR YBIR?")
+console.log(rot13("SERR PBQR PNZC"))
 
 // console.log(rot13("SERR CVMMN!"))
 
