1566ab8a314bf66990338b602557324ea4ba5971
aniebodavid <aniebovictor001@gmail.com>
2022-04-22 03:29:38 -0700

    Solve spinword algorithms

--- Stop gninnipS My sdroW!/index.js
+++ Stop gninnipS My sdroW!/index.js
@@ -1,34 +1,23 @@
 // Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
-// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" ) => returns "This is rehtona test"
 
 function spinWords(string) {
-  //TODO Have fun :)
+  let newStr = []
+  let splitStr = string.split(" ")
+  for (let i = 0; i < splitStr.length; i++) {
+    if (splitStr[i].length >= 5) {
+      splitStr[i] = splitStr[i].split("").reverse().join("")
+    }
+  }
+  return splitStr.join(" ")
 }
+console.log(spinWords("Stop spinning my words"))
 
-const chai = require("chai")
-const assert = chai.assert
-
-describe("Spinning words", () => {
-  it("Sample tests", () => {
-    assert.strictEqual(spinWords("Welcome"), "emocleW")
-    assert.strictEqual(spinWords("Hey fellow warriors"), "Hey wollef sroirraw")
-    assert.strictEqual(spinWords("This is a test"), "This is a test")
-    assert.strictEqual(
-      spinWords("This is another test"),
-      "This is rehtona test"
-    )
-    assert.strictEqual(
-      spinWords("You are almost to the last test"),
-      "You are tsomla to the last test"
-    )
-    assert.strictEqual(
-      spinWords("Just kidding there is still one more"),
-      "Just gniddik ereht is llits one more"
-    )
-    assert.strictEqual(
-      spinWords("Seriously this is the last one"),
-      "ylsuoireS this is the last one"
-    )
-  })
-})
+// spinWords("Welcome") => "emocleW"
+// spinWords("Hey fellow warriors") =>"Hey wollef sroirraw"
+// (spinWords("This is a test") => "This is a test"
+// spinWords("This is another test") =>"This is rehtona test"
+// spinWords("You are almost to the last test") =>"You are tsomla to the last test"
+// spinWords("Just kidding there is still one more") => "Just gniddik ereht is llits one more"
+// spinWords("Seriously this is the last one") => "ylsuoireS this is the last one"
