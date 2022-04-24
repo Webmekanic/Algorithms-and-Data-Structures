60598d6cd805a9d33687181686fa22ee549a60a7
aniebodavid <aniebovictor001@gmail.com>
2022-04-24 09:27:12 -0700

    Add new codewars katas

---
+++ count of positive/index.js
@@ -0,0 +1,30 @@
+// Count of positives / sum of negatives
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+function countPositivesSumNegatives(input) {
+    return ?;
+}
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Example tests", () => {
+//   it("Testing for fixed test 1", () => {
+//     let testDa;ta = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+//     let actual = countPositivesSumNegatives(testData);
+//     let expected = [10, -65];
+//     assert.deepEqual(actual, expected)
+//   })
+    
+//   it("Testing for fixed test 2", () => {
+//     let testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+//     let actual = countPositivesSumNegatives(testData);
+//     let expected = [8, -50];    
+//     assert.deepEqual(actual, expected);
+//   });
+// });
