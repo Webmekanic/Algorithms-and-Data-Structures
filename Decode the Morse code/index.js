b2847d77864ba1cfa93867fd33f4fea1de54b1a6
aniebodavid <aniebovictor001@gmail.com>
2022-04-27 01:28:30 -0700

    solve decode the morse code codewars kata

--- Decode the Morse code/index.js
+++ Decode the Morse code/index.js
@@ -22,9 +22,61 @@
 
 // Good luck!
 
-decodeMorse = function (morseCode) {
- 
+let MORSE_CODE = {
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "-.-.--": "!",
+  ".-.-.-": ".",
+  "--..--": ",",
+}
+
+const decodeMorse = function (morseCode) {
+  return morseCode
+    .split("   ") // get word code 3 spaces apart
+    .map((word) =>
+      word
+        .split(" ") // get character code 1 spaces apart
+        .map((character) => MORSE_CODE[character]) // decode Morse code character
+        .join("")
+    )
+    .join(" ") // add spaces between words
+    .trim()
 }
 
 // TEST
-// decodeMorse(".... . -.--   .--- ..- -.. ."), "HEY JUDE"
+console.log(decodeMorse(".- -. .. . -... ---"))
+// => "HEY JUDE"
