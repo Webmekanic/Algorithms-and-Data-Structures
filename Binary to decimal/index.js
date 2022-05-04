725f6779b60e822ed79299fcc152762273f05049
aniebodavid <aniebovictor001@gmail.com>
2022-05-04 10:35:59 -0700

    solve convert bin to decimal

--- Binary to decimal/index.js
+++ Binary to decimal/index.js
@@ -1,17 +1,6 @@
 // Complete the function which converts a binary number (given as a string) to a decimal number.
 function binToDec(bin) {
-  let splitstr = bin.split("")
-  let convertToDeci = 0
-  for (let i = 0; i < splitstr.length; i++) {
-    const convertStrToNum = Number(splitstr[i] * 2)
-    const strPow = splitstr.length - 1 - i
-    // console.log(Math.pow(convertStrToNum, strPow))
-    convertToDeci += Math.pow(convertStrToNum, strPow)
-    // console.log(Math.pow(0, 0))
-
-    // console.log(convertToDeci)
-  }
-  return convertToDeci
+  return parseInt(bin, 2)
 }
 console.log(binToDec("11001010"))
 // 11001010
