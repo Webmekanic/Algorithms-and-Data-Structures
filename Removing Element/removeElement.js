05fddcd1838c18bfd7ca62871fb481ef3b0b1da6
aniebodavid <aniebovictor001@gmail.com>
2022-06-07 09:34:54 -0700

    solve remove element codewars kata

--- Removing Element/removeElement.js
+++ Removing Element/removeElement.js
@@ -7,10 +7,8 @@
 // None of the arrays will be empty, so you don't have to worry about that!
 
 function removeEveryOther(arr) {
-  let newArr = []
-  for (let i = 0; i < arr.length; i++) {}
-
   // console.log(newArr)
+  return arr.filter((_, i) => i % 2 === 0)
 }
 console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 // describe("Basic tests",() =>{
