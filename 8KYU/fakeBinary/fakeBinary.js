35f73bc2695e08386b80cf3a0c11e4cb59085bb1
aniebodavid <aniebovictor001@gmail.com>
2022-06-23 14:32:26 -0700

    solve fakebin codewars kata

---
+++ 8KYU/fakeBinary/fakeBinary.js
@@ -0,0 +1,15 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// PREP
+
+// Replace number below five with 0
+// Replace number from above five with 1
+
+// Note: input will never be an empty string
+function fakeBin(x) {
+  return x
+    .split("")
+    .map((x) => (x < 5 ? 0 : 1))
+    .join("")
+}
+console.log(fakeBin("45385593107843568"))
