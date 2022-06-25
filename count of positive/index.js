226b1a45cd4abfac34cd7d124ceb61a6cd7d69e0
aniebodavid <aniebovictor001@gmail.com>
2022-06-25 05:59:19 -0700

    solve count positive and sum negative codewars

--- count of positive/index.js
+++ count of positive/index.js
@@ -6,8 +6,20 @@
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 function countPositivesSumNegatives(input) {
-    return ?;
+  let positiveNum = 0
+  let negativeNum = 0
+  if (input === null || input.length === 0) {
+    return []
+  } else {
+    input.forEach((num) => (num > 0 ? positiveNum++ : (negativeNum += num)))
+  }
+  return [positiveNum, negativeNum]
 }
+console.log(
+  countPositivesSumNegatives([
+    0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
+  ])
+)
 
 // const chai = require("chai");
 // const assert = chai.assert;
@@ -20,11 +32,11 @@ function countPositivesSumNegatives(input) {
 //     let expected = [10, -65];
 //     assert.deepEqual(actual, expected)
 //   })
-    
+
 //   it("Testing for fixed test 2", () => {
 //     let testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
 //     let actual = countPositivesSumNegatives(testData);
-//     let expected = [8, -50];    
+//     let expected = [8, -50];
 //     assert.deepEqual(actual, expected);
 //   });
 // });
