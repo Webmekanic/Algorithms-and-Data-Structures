eaa098bb79479106d232f391623d7f9c3670495d
aniebodavid <aniebovictor001@gmail.com>
2022-08-26 07:45:22 -0700

    solve multiplying two numbers codewars kata

---
+++ 8KYU/multiplyTwoNum/multiplyTwoNum.js
@@ -0,0 +1,17 @@
+// Function 3 - multiplying two numbers
+
+// Implement a function called multiply, which takes two numbers and returns their product:
+
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
+
+// Write here your multiply-function
+function multiply(num, value) {
+  return num * value
+  // console.log(typeof num)
+}
+
+console.log(multiply(2, 3))
+
+// => 6
