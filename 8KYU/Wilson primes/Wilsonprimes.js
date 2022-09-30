cb226fe3ea0da948bafbfb81ab6a1e78b2c8a0ec
aniebodavid <aniebovictor001@gmail.com>
2022-09-30 01:33:52 -0700

    solve wilson prime using array from

--- 8KYU/Wilson primes/Wilsonprimes.js
+++ 8KYU/Wilson primes/Wilsonprimes.js
@@ -1,8 +1,13 @@
-function amIWilson(P) {
-  return ((factorial(P - 1) + 1) / Math.pow(P, 2)) % 1 === 0
-}
-
-function factorial(n) {
-  if (n == 0) return 1
-  else return n * factorial(n - 1)
+// solve using array from
+function amIWilson(p) {
+  return (
+    ((Array.from({ length: p - 1 }, (_, index) => index + 1).reduce(
+      (prev, cur) => prev * cur,
+      1
+    ) +
+      1) /
+      (p * p)) %
+      1 ==
+    0
+  )
 }
