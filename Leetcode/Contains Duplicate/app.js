378780bf550a026210e9b8052c3ff39c435268db
aniebodavid <aniebovictor001@gmail.com>
2023-01-06 08:54:55 -0800

    solved contains duplicate challenge

--- Leetcode/Contains Duplicate/app.js
+++ Leetcode/Contains Duplicate/app.js
@@ -35,3 +35,28 @@ console.log([1, 2, 3, 1])
 // true
 
 
+/**
+ * Sort - HeapSort Space O(1) | QuickSort Space O(log(N))
+ * Time O(N * log(N)) | Space O(1)
+ * https://leetcode.com/problems/contains-duplicate/
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = (nums) => {
+    nums.sort((a, b) => a - b);/* Time O(N * log(N)) | Space O(1 || log(N)) */
+
+    return hasDuplicate(nums);
+}
+
+const hasDuplicate = (nums) => {
+    for (let curr = 0; curr < (nums.length - 1); curr++) {/* Time O(N) */
+        const next = (curr + 1);
+
+        const isNextDuplicate = nums[curr] === nums[next];
+        if (isNextDuplicate) return true;
+    }
+
+    return false;
+}
+
+
